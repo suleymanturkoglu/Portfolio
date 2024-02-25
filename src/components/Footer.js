68c4b9c0dc34215c837a88c6fb381399c218f238
suleymanturkoglu <suleymanturkoglu_@hotmail.com>
2024-02-25 20:33:48 +0300

    ...
    Signed-off-by: suleymanturkoglu <suleymanturkoglu_@hotmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/suleymanturkoglu/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
