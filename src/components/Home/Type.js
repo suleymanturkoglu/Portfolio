57d6b1b1f8925d3aacb89d0e0143576051f89c7f
Süleyman TÜRKOĞLU <suleymanturkoglu_@hotmail.com>
2024-03-06 18:41:58 +0300

    ....
    Signed-off-by: Süleyman TÜRKOĞLU <suleymanturkoglu_@hotmail.com>

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -10,7 +10,6 @@ function Type() {
           "Freelancer",
           "Custom CAD/CAM Developer",
           "SolidWorks Addin Developer",
-          "Open Source Contributor",
         ],
         autoStart: true,
         loop: true,
