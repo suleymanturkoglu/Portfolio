f78becbc8e190a045865072521d30757ffe93d7c
Süleyman TÜRKOĞLU <suleymanturkoglu_@hotmail.com>
2024-03-06 11:44:51 +0300

    ....

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,7 @@ function Type() {
         strings: [
           "Software Developer",
           "Freelancer",
-          "MERN Stack Developer",
+          "Custom CAD/CAM Developer",
           "Open Source Contributor",
         ],
         autoStart: true,
