3c14c739e474a08a592b4dfc804f6221e4bbb5f9
Süleyman TÜRKOĞLU <suleymanturkoglu_@hotmail.com>
2024-03-21 13:15:53 +0300

    projects changed
    Signed-off-by: Süleyman TÜRKOĞLU <suleymanturkoglu_@hotmail.com>

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -3,7 +3,7 @@ import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
-  DiReact,
+  DiAngular,
   DiNodejs,
   DiMongodb,
   DiPython,
@@ -17,7 +17,7 @@ import {
   SiSolidity,
   SiPostgresql,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+import { TbBrandThreejs } from "react-icons/tb";
 
 function Techstack() {
   return (
@@ -26,34 +26,28 @@ function Techstack() {
         <CgCPlusPlus />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
+        <SiChasrp />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
+        <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
+        <DiAngular />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
+        <TbBrandThreejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
